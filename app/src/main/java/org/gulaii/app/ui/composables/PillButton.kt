e766e9dc5dea832a5d13fed9d9027d9e97a02cad
Shir5 <Vova0120_black@icloud.com>
2025-04-09 18:35:28 +0300

    reusable pill button composable & initial screen mock

--- app/src/main/java/org/gulaii/app/ui/composables/PillButton.kt
+++ app/src/main/java/org/gulaii/app/ui/composables/PillButton.kt
@@ -1,35 +1,58 @@
 package org.gulaii.app.ui.composables
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.gulaii.app.ui.theme.GulaiiTheme
-
+import org.gulaii.app.ui.theme.Typography
 
 @Composable
-fun PillButton(modifier: Modifier = Modifier, isEnabled: Boolean, buttonText: String, clickAction: () -> Unit) {
+fun PillButton(
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean,
+    buttonColor: ButtonColors,
+    clickAction: () -> Unit,
+    content: @Composable () -> Unit
+) {
     ElevatedButton(
         onClick = clickAction,
-        modifier = modifier.padding(16.dp),
+        modifier = modifier,
         enabled = isEnabled,
-    ){
-        Text(text = buttonText)
+        colors = buttonColor,
+    ) {
+        content()
     }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PillButtonPreview(){
+fun PillButtonPreview() {
     GulaiiTheme {
         PillButton(
             isEnabled = true,
-            buttonText = "vova",
-            clickAction = { }
+            clickAction = { },
+            buttonColor = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = Modifier
+                .padding(16.dp)
+                .width(200.dp)
+                .height(56.dp),
+            content = {
+                Text(
+                    text = "vova",
+                    style = Typography.bodyLarge.copy(color = Color.White)
+                )
+            }
         )
     }
 }
--- app/src/main/java/org/gulaii/app/ui/screens/initialScreen/InitialScreen.kt
+++ app/src/main/java/org/gulaii/app/ui/screens/initialScreen/InitialScreen.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.gulaii.app.R
+import org.gulaii.app.ui.composables.PillButton
 import org.gulaii.app.ui.theme.GulaiiTheme
 
 class InitialScreen : ComponentActivity() {
@@ -20,7 +26,7 @@ class InitialScreen : ComponentActivity() {
         setContent {
             GulaiiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
+                    Screen(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +37,68 @@ class InitialScreen : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TextColum(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = stringResource(R.string.initial_screen_main_header),
+            fontSize = 24.sp
+        )
+        Text(text = stringResource(R.string.initial_screen_secondary_header))
+    }
+}
+
+@Composable
+fun Screen(name: String, modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        // Центрированный текст
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextColum()
+        }
+
+        // Кнопка снизу
+        PillButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 64.dp)
+                .height(60.dp)
+                .width(200.dp),
+            isEnabled = true,
+            buttonColor = ButtonDefaults.buttonColors(colorResource(R.color.purple_500)),
+            clickAction = { println("vova") }
+        ) {
+            Text(
+                text = "Welcome!",
+                color = Color.White,
+                style = MaterialTheme.typography.labelLarge,
+                fontSize = 24.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
+fun TextColumnPreview() {
     GulaiiTheme {
-        Greeting2("Android")
+        TextColum()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    GulaiiTheme {
+        Screen("Android")
     }
 }
