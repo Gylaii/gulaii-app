c23d8bfa9cba84001e9454d2f6eefeb2167c0769
Vladimir.D <61351477+Shir5@users.noreply.github.com>
2025-04-12 20:46:09 +0300

    feat: color & typography modification (#17)
    * feat: ✨ color & typography modification
    
    * feat: ✨ color & typography modification

--- app/src/main/java/org/gulaii/app/ui/theme/Color.kt
+++ app/src/main/java/org/gulaii/app/ui/theme/Color.kt
@@ -2,6 +2,19 @@ package org.gulaii.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val PrimaryGreen = Color(0xFF2B6C4F)
+val SecondaryGreen = Color(0xFF67AE6E)
+val SoftGreen = Color(0xFFE9FBC4)
+val AccentGreen = Color(0xFFBFFF3C)
+
+val PrimaryBlue = Color(0xFF4D55CC)
+val SecondaryBlue = Color(0xFF60B5FF)
+val SoftBlue = Color(0xFFAFDDFF)
+val AccentBlue = Color(0xFF261FB3)
+
+val PrimaryOrange = Color(0xFFEA7300)
+val SecondaryOrange = Color(0xFFFFA955)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +22,4 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
--- app/src/main/java/org/gulaii/app/ui/theme/Type.kt
+++ app/src/main/java/org/gulaii/app/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package org.gulaii.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.gulaii.app.R.font.futura_heavy
+import org.gulaii.app.R.font.futura_light
+import org.gulaii.app.R.font.futura_medium
 
-// Set of Material typography styles to start with
+val FuturaFamily = FontFamily(
+    Font(futura_heavy, weight = FontWeight.Bold),
+    Font(futura_medium, weight = FontWeight.Medium),
+    Font(futura_light, weight = FontWeight.Light)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FuturaFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = FuturaFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FuturaFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
 )
