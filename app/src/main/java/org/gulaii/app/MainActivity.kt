b21750e629271b604e6bcb27f0975c1468da0f8e
Shir5 <Vova0120_black@icloud.com>
2025-04-18 14:10:19 +0300

    feat: ✨ add screen navigation system

--- app/src/main/java/org/gulaii/app/MainActivity.kt
+++ app/src/main/java/org/gulaii/app/MainActivity.kt
@@ -1,10 +1,10 @@
 package org.gulaii.app
 
+import AppNavHost
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import org.gulaii.app.ui.navigation.AppNavHost
 import org.gulaii.app.ui.theme.GulaiiTheme
 
 class MainActivity : ComponentActivity() {
