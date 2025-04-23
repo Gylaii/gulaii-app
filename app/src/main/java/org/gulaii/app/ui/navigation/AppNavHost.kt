02f6bee2598cb7605515c2c6e9dc11e52547de64
Shir5 <Vova0120_black@icloud.com>
2025-04-23 14:23:15 +0300

    refactor: ♻️ Navigation system refactoring

--- app/src/main/java/org/gulaii/app/ui/navigation/AppNavHost.kt
+++ app/src/main/java/org/gulaii/app/ui/navigation/AppNavHost.kt
@@ -4,45 +4,50 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import org.gulaii.app.ui.navigation.AuthGraph
+import org.gulaii.app.ui.navigation.InitialGraph
+import org.gulaii.app.ui.navigation.OnboardingGraph
 import org.gulaii.app.ui.navigation.Screen
+import org.gulaii.app.ui.screens.authScreen.AuthScreenView
 import org.gulaii.app.ui.screens.initialScreen.InitialScreenView
 import org.gulaii.app.ui.screens.onboardingScreen.OnboardingView
 
 @Composable
 fun AppNavHost(
-  navController: NavHostController,
-  modifier: Modifier = Modifier,
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
 ) {
-  NavHost(
-    navController = navController,
-    startDestination = "initial_graph", // Начинаем с вложенного графа Initial
-    modifier = modifier
-  ) {
-    navigation(
-      startDestination = Screen.Initial.route,
-      route = "initial_graph"
+    NavHost(
+        navController = navController,
+        startDestination = InitialGraph,
+        modifier = modifier
     ) {
-      composable(route = Screen.Initial.route) {
-        InitialScreenView(
-          onNext = {
-            navController.navigate("onboarding_graph")
-          }
-        )
-      }
-    }
+        //Initial Navigation Graph
+        navigation<InitialGraph>(startDestination = Screen.Initial) {
+            composable<Screen.Initial> {
+                InitialScreenView(
+                    onNext = { navController.navigate(OnboardingGraph) }
+                )
+            }
+        }
 
-    // Вложенный граф для онбординга
-    navigation(
-      startDestination = Screen.Onboarding.route,
-      route = "onboarding_graph"
-    ) {
-      composable(route = Screen.Onboarding.route) {
-        OnboardingView(
-          onFinished = {
-            navController.navigate("initial_graph")
-          }
-        )
-      }
+        //Onboarding Navigation Graph
+        navigation<OnboardingGraph>(startDestination = Screen.Onboarding) {
+            composable<Screen.Onboarding> {
+                OnboardingView(
+                    onFinished = { navController.navigate(Screen.Auth) }
+                )
+            }
+        }
+
+        //Auth Navigation Graph
+        navigation<AuthGraph>(startDestination = Screen.Auth) {
+            composable<Screen.Auth> {
+                AuthScreenView(
+                    onSubmit = { navController.navigate(Screen.Onboarding) }
+                )
+            }
+        }
     }
-  }
 }
+
