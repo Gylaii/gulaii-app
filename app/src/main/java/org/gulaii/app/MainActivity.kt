de59564bd36574845ba086f5ea48a12eb40bdafe
Shir5 <Vova0120_black@icloud.com>
2025-04-25 16:30:53 +0300

    refactor: :recycle: navigation refactor considering auth screen
    Navigation update considering Auth Screen

--- app/src/main/java/org/gulaii/app/MainActivity.kt
+++ app/src/main/java/org/gulaii/app/MainActivity.kt
@@ -1,6 +1,6 @@
 package org.gulaii.app
 
-import AppNavHost
+import org.gulaii.app.ui.navigation.AppNavHost
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
