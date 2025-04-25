afcc4f98b0ec9aa90309e7c802f12f9377c235c5
Shir5 <Vova0120_black@icloud.com>
2025-04-25 16:30:53 +0300

    refactor: :recycle: navigation refactor considering auth screen

--- app/src/main/java/org/gulaii/app/ui/navigation/AppNavHost.kt
+++ app/src/main/java/org/gulaii/app/ui/navigation/AppNavHost.kt
@@ -1,53 +1,57 @@
+package org.gulaii.app.ui.navigation
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
-import org.gulaii.app.ui.navigation.AuthGraph
-import org.gulaii.app.ui.navigation.InitialGraph
-import org.gulaii.app.ui.navigation.OnboardingGraph
-import org.gulaii.app.ui.navigation.Screen
 import org.gulaii.app.ui.screens.authScreen.AuthScreenView
 import org.gulaii.app.ui.screens.initialScreen.InitialScreenView
 import org.gulaii.app.ui.screens.onboardingScreen.OnboardingView
+import org.gulaii.app.ui.screens.recoveryScreen.RecoveryView
 
 @Composable
 fun AppNavHost(
-    navController: NavHostController,
-    modifier: Modifier = Modifier,
+  navController: NavHostController,
+  modifier: Modifier = Modifier,
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = InitialGraph,
-        modifier = modifier
-    ) {
-        //Initial Navigation Graph
-        navigation<InitialGraph>(startDestination = Screen.Initial) {
-            composable<Screen.Initial> {
-                InitialScreenView(
-                    onNext = { navController.navigate(OnboardingGraph) }
-                )
-            }
-        }
-
-        //Onboarding Navigation Graph
-        navigation<OnboardingGraph>(startDestination = Screen.Onboarding) {
-            composable<Screen.Onboarding> {
-                OnboardingView(
-                    onFinished = { navController.navigate(Screen.Auth) }
-                )
-            }
-        }
-
-        //Auth Navigation Graph
-        navigation<AuthGraph>(startDestination = Screen.Auth) {
-            composable<Screen.Auth> {
-                AuthScreenView(
-                    onSubmit = { navController.navigate(Screen.Onboarding) }
-                )
-            }
-        }
+  NavHost(
+    navController = navController,
+    startDestination = InitialGraph,
+    modifier = modifier
+  ) {
+    //Initial Navigation Graph
+    navigation<InitialGraph>(startDestination = Screen.Initial) {
+      composable<Screen.Initial> {
+        InitialScreenView(
+          onNext = { navController.navigate(OnboardingGraph) }
+        )
+      }
     }
-}
 
+    //Onboarding Navigation Graph
+    navigation<OnboardingGraph>(startDestination = Screen.Onboarding) {
+      composable<Screen.Onboarding> {
+        OnboardingView(
+          onFinished = { navController.navigate(Screen.Auth) }
+        )
+      }
+    }
+
+    //Auth Navigation Graph
+    navigation<AuthGraph>(startDestination = Screen.Auth) {
+      composable<Screen.Auth> {
+        AuthScreenView(
+          onForgotPasswordClick = { navController.navigate(Screen.Recovery) },
+          onSubmit = { navController.navigate(Screen.Onboarding) }
+        )
+      }
+      composable<Screen.Recovery> {
+        RecoveryView(
+        )
+      }
+
+    }
+  }
+}
